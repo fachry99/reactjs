0a3ddccc9bb52e00ce2af12fcbb9fb5842218f90
fachry99 <151fachry@gmail.com>
2023-09-07 21:54:10 +0700

    mencoba build sistem dengan vite

---
+++ instalasi & konfigurasi react/src/components/header.jsx
@@ -0,0 +1,6 @@
+/* eslint-disable react/prop-types */
+export default function Header({ contoh }) {
+  return (
+    <h1> ini versi menggunakan function {contoh ? contoh : "komponen"}🚀</h1>
+  );
+}
