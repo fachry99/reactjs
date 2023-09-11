b3e477b78af26341889a1907db54305002f53de8
fachry99 <151fachry@gmail.com>
2023-09-11 20:40:30 +0700

    CRUD: simple To do list app

--- basic-knowledge/src/App.jsx
+++ basic-knowledge/src/App.jsx
@@ -1,17 +1,62 @@
+/* eslint-disable react/jsx-key */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import { useState } from "react";
 import "./App.css";
+import { Task } from "./task.jsx";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const addTask = () => {
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskName: newTask,
+      Completed: false,
+    };
+    setTodoList([...todoList, task]);
+  };
+
+  const DeleteTask = (id) => {
+    setTodoList(todoList.filter((task) => task.id !== id));
+  };
+
+  const CompleteTask = (id) => {
+    setTodoList(
+      todoList.map((task) => {
+        if (task.id === id) {
+          return { ...task, Completed: true };
+        } else {
+          return task;
+        }
+      })
+    );
+  };
 
   return (
-    <div>
-      <button onClick={() => setCount(count + 1)}>Increase</button>
-      <button onClick={() => setCount(count - 1)}>Decrease</button>
-      <button onClick={() => setCount(0)}>Set To zero</button>
-      {count}
+    <div className="App">
+      <div className="addTask">
+        <input onChange={handleChange} />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <div className="list">
+        {todoList.map((task) => {
+          return (
+            <Task
+              taskName={task.taskName}
+              id={task.id}
+              Completed={task.Completed}
+              DeleteTask={DeleteTask}
+              CompleteTask={CompleteTask}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
