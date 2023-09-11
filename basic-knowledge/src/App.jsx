163b4150bc5804a6b04390829e102b01a18759ff
fachry99 <151fachry@gmail.com>
2023-09-11 21:57:41 +0700

    belajar useEffect sederhana

--- basic-knowledge/src/App.jsx
+++ basic-knowledge/src/App.jsx
@@ -3,60 +3,23 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
 import "./App.css";
-import { Task } from "./task.jsx";
+//import { Task } from "./task.jsx";
+import { Text } from "./Text.jsx";
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
-  const [newTask, setNewTask] = useState("");
-
-  const handleChange = (event) => {
-    setNewTask(event.target.value);
-  };
-
-  const addTask = () => {
-    const task = {
-      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
-      taskName: newTask,
-      Completed: false,
-    };
-    setTodoList([...todoList, task]);
-  };
-
-  const DeleteTask = (id) => {
-    setTodoList(todoList.filter((task) => task.id !== id));
-  };
-
-  const CompleteTask = (id) => {
-    setTodoList(
-      todoList.map((task) => {
-        if (task.id === id) {
-          return { ...task, Completed: true };
-        } else {
-          return task;
-        }
-      })
-    );
-  };
+  const [showtext, setShowtext] = useState(false);
 
   return (
     <div className="App">
-      <div className="addTask">
-        <input onChange={handleChange} />
-        <button onClick={addTask}>Add Task</button>
-      </div>
-      <div className="list">
-        {todoList.map((task) => {
-          return (
-            <Task
-              taskName={task.taskName}
-              id={task.id}
-              Completed={task.Completed}
-              DeleteTask={DeleteTask}
-              CompleteTask={CompleteTask}
-            />
-          );
-        })}
-      </div>
+      <button
+        onClick={() => {
+          setShowtext(!showtext);
+        }}
+      >
+        Show Text
+      </button>
+
+      {showtext && <Text />}
     </div>
   );
 }
---
+++ basic-knowledge/src/Text.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+
+export const Text = () => {
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    console.log("Component Mounted");
+
+    return () => {
+      console.log("Component Unmounted");
+    };
+  });
+
+  return (
+    <div>
+      <input
+        onChange={(event) => {
+          setText(event.target.value);
+        }}
+      ></input>
+
+      <h1>{text}</h1>
+    </div>
+  );
+};
