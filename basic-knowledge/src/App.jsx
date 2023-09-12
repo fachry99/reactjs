7d5229d1ae101cfcc25139d636137f776b587c7f
fachry99 <151fachry@gmail.com>
2023-09-12 09:08:26 +0700

    delete some  file + cara simple get API

--- basic-knowledge/src/App.jsx
+++ basic-knowledge/src/App.jsx
@@ -1,25 +1,18 @@
-/* eslint-disable react/jsx-key */
-/* eslint-disable no-unused-vars */
-/* eslint-disable react/prop-types */
+// eslint-disable-next-line no-unused-vars
 import { useState } from "react";
 import "./App.css";
-//import { Task } from "./task.jsx";
-import { Text } from "./Text.jsx";
+
+fetch("https://catfact.ninja/fact").then((response) =>
+  response.json().then((data) => {
+    console.log(data);
+  })
+);
 
 function App() {
-  const [showtext, setShowtext] = useState(false);
-
   return (
     <div className="App">
-      <button
-        onClick={() => {
-          setShowtext(!showtext);
-        }}
-      >
-        Show Text
-      </button>
-
-      {showtext && <Text />}
+      <button>Generate Cat Fact</button>
+      <p></p>
     </div>
   );
 }
