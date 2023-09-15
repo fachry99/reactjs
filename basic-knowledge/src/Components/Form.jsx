f192979755821470eb71e05e09ab30a63b431816
fachry99 <151fachry@gmail.com>
2023-09-15 20:22:51 +0700

    add resolver dengan packedge hookfrom

--- basic-knowledge/src/Components/Form.jsx
+++ basic-knowledge/src/Components/Form.jsx
@@ -1,12 +1,64 @@
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
 export const Form = () => {
+  const schema = yup.object().shape({
+    fullName: yup.string().required("your Full Name is required"),
+    email: yup.string().email().required(),
+    age: yup.number().positive().integer().min(18).required(),
+    password: yup.string().min(8).max(15).required(),
+    confrimPassword: yup
+      .string()
+      .oneOf([yup.ref("password"), null], "Passwords must match")
+      .required(),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+  const onSubmit = (data) => console.log(data);
+
   return (
-    <form>
-      <input type="text" placeholder="Full Name..." />
-      <input type="email" placeholder="Email..." />
-      <input type="number" placeholder="Age..." />
-      <input type="password" placeholder="Password..." />
-      <input type="password" placeholder="Confirm Password..." />
-      <button type="submit">Submit</button>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        type="text"
+        placeholder="Full Name..."
+        className="form-control"
+        {...register("fullName")}
+      />
+      {errors.fullName && <p>{errors.fullName.message}</p>}
+      <input
+        type="email"
+        placeholder="Email..."
+        className="form-control"
+        {...register("email")}
+      />
+      <input
+        type="number"
+        placeholder="Age..."
+        className="form-control"
+        {...register("age")}
+      />
+      <input
+        type="password"
+        placeholder="Password..."
+        className="form-control"
+        {...register("password")}
+      />
+      <input
+        type="password"
+        placeholder="Confirm Password..."
+        className="form-control"
+        {...register("confrimPassword")}
+      />
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
     </form>
   );
 };
