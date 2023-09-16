aa64c02f588efbbf565c9b40dec54e662274a073
fachry99 <151fachry@gmail.com>
2023-09-16 07:25:30 +0700

    custom hooks

--- basic-knowledge/src/App.jsx
+++ basic-knowledge/src/App.jsx
@@ -1,12 +1,22 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
 import "./App.css";
-import { Form } from "./Components/Form";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useToggle } from "./useToggle";
 
 function App() {
+  const [isVisible, toggle] = useToggle();
+  const [isVisible2, toggle2] = useToggle();
   return (
     <div className="App">
-      <Form />
+      <button className="btn btn-primary" onClick={toggle}>
+        {isVisible ? "Hide" : "Show"}
+      </button>
+      {isVisible && <h1>Hidden text here</h1>}
+
+      <button className="btn btn-secondary" onClick={toggle2}>
+        {isVisible2 ? "Hide" : "Show"}
+      </button>
+      {isVisible2 && <h1>Hidden text here</h1>}
     </div>
   );
 }
