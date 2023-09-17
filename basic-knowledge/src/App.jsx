5c5f15ecd9851356d9462d5c414f40e45f95db85
fachry99 <151fachry@gmail.com>
2023-09-17 18:17:20 +0700

    menampilkan data seorang

--- basic-knowledge/src/App.jsx
+++ basic-knowledge/src/App.jsx
@@ -1,17 +1,17 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useCounter } from "./useCounter";
-
+import { Person } from "./Components/person";
 function App() {
-  const [count, increment, decrement, reset] = useCounter(40);
-
   return (
     <div className="App">
-      {count}
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-      <button onClick={reset}>Reset</button>
+      <Person
+        name="Fachry"
+        email="Fachry151@gmail.com"
+        age={20}
+        isMarried={false}
+        friends={["aldi", "dono", "dodi"]}
+      />
     </div>
   );
 }
