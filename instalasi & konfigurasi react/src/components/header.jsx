8a8e49db1175866b39647cba11ffc11df1077ec3
fachry99 <151fachry@gmail.com>
2023-09-19 06:19:28 +0700

    Update file

--- instalasi & konfigurasi react/src/components/header.jsx
+++
@@ -1,6 +0,0 @@
-/* eslint-disable react/prop-types */
-export default function Header({ contoh }) {
-  return (
-    <h1> ini versi menggunakan function {contoh ? contoh : "komponen"}🚀</h1>
-  );
-}
