27ddaa5629d61f261b79c208ce0c66029f0d26e0
fachry99 <151fachry@gmail.com>
2023-09-21 11:30:44 +0700

    menambahkan fitur like, unlike button yang terintergrasi dengan firebase data

--- firebase-project/src/pages/main/post.tsx
+++ firebase-project/src/pages/main/post.tsx
@@ -1,19 +1,107 @@
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db } from "../../config/firebase";
 import { Post as IPost } from "./Home";
+import {
+  collection,
+  addDoc,
+  query,
+  getDocs,
+  where,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
+import { useEffect, useState } from "react";
 
-interface PostProps {
+interface Props {
   post: IPost;
 }
-export const Post = (props: PostProps) => {
+
+interface Like {
+  userId: string;
+  likeId: string;
+}
+export const Post = (props: Props) => {
   const { post } = props;
+  const [user] = useAuthState(auth);
+
+  const [likes, setLikes] = useState<Like[] | null>(null);
+
+  const likesRef = collection(db, "likes");
+
+  const likesDoc = query(likesRef, where("postId", "==", post.id));
+
+  const getLikes = async () => {
+    const data = await getDocs(likesDoc);
+    setLikes(
+      data.docs.map((doc) => ({ userId: doc.data().userId, likeId: doc.id }))
+    );
+  };
+  const addLike = async () => {
+    try {
+      const newDoc = await addDoc(likesRef, {
+        userId: user?.uid,
+        postId: post.id,
+      });
+      if (user) {
+        setLikes((prev) =>
+          prev
+            ? [...prev, { userId: user.uid, likeId: newDoc.id }]
+            : [{ userId: user.uid, likeId: newDoc.id }]
+        );
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const removeLike = async () => {
+    try {
+      const likeToDeleteQuery = query(
+        likesRef,
+        where("postId", "==", post.id),
+        where("userId", "==", user?.uid)
+      );
+
+      const likeToDeleteData = await getDocs(likeToDeleteQuery);
+      const likeId = likeToDeleteData.docs[0].id;
+      const likeToDelete = doc(db, "likes", likeId);
+      await deleteDoc(likeToDelete);
+      if (user) {
+        setLikes(
+          (prev) => prev && prev.filter((like) => like.likeId !== likeId)
+        );
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const hasUserLiked = likes?.find((like) => like.userId === user?.uid);
+
+  useEffect(() => {
+    getLikes();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div>
       <div className="title">
-        <h1>{post.title}</h1>
+        <h1> {post.title}</h1>
       </div>
       <div className="body">
-        <p>{post.description}</p>
+        <p> {post.description} </p>
       </div>
-      <footer>@{post.username}</footer>
+
+      <div className="footer">
+        <p> @{post.username} </p>
+        <button
+          className="btn btn-dark"
+          onClick={hasUserLiked ? removeLike : addLike}
+        >
+          {hasUserLiked ? <>&#128078;</> : <>&#128077;</>}{" "}
+        </button>
+        {likes && <p> Likes: {likes?.length} </p>}
+      </div>
     </div>
   );
 };
