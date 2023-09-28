bc760021988f71ab78d2eb77bf9e0ade99b294c2
fachry99 <151fachry@gmail.com>
2023-09-28 09:22:05 +0700

    useState with object

--- hello-world/src/App.jsx
+++ hello-world/src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import HookCounterThree from "./components/HookCounterThree";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <HookCounterThree />
+    </div>
+  );
 }
 
 export default App;
--- hello-world/src/components/HookCounterThree.jsx
+++ hello-world/src/components/HookCounterThree.jsx
@@ -2,7 +2,25 @@ import { useState } from "react";
 
 function HookCounterThree() {
   const [name, setName] = useState({ firstName: "", lastName: "" });
-  return <div>HookCounterThree</div>;
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder="First Name .."
+        value={name.firstName}
+        onChange={(e) => setName({ ...name, firstName: e.target.value })}
+      ></input>
+      <input
+        type="text"
+        placeholder="Last Name .."
+        value={name.lastName}
+        onChange={(e) => setName({ ...name, lastName: e.target.value })}
+      ></input>
+      <h2>Your first name is - {name.firstName}</h2>
+      <h2>Your last name is - {name.lastName}</h2>
+      <h2>{JSON.stringify(name)}</h2>
+    </form>
+  );
 }
 
 export default HookCounterThree;
