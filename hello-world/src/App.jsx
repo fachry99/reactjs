7109525eeb1c245cd9067839005d7362b42b3c49
fachry99 <151fachry@gmail.com>
2023-10-01 11:13:08 +0700

    useReducer(complex state & action) in file name: CounterTwo

--- hello-world/src/App.jsx
+++ hello-world/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
-import CounterOne from "./components/CounterOne";
+import CounterTwo from "./components/CounterTwo";
+// import CounterOne from "./components/CounterOne";
 
 function App() {
   return (
     <div className="App">
-      <CounterOne />
+      <CounterTwo />
+      {/* <CounterOne /> */}
     </div>
   );
 }
