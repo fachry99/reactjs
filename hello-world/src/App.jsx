20d175184b0e4a3f2b734916e683f2a49b4b4db1
fachry99 <151fachry@gmail.com>
2023-10-02 11:33:47 +0700

    refersh branch

--- hello-world/src/App.jsx
+++ hello-world/src/App.jsx
@@ -1,22 +1,7 @@
 import "./App.css";
-import DataFetchingTwo from "./components/DataFetchingTwo";
-// import DataFetchingOne from "./components/DataFetchingOne";
-// import React, { useReducer } from "react";
-// import ComponentA from "./components/ComponentA";
-// import ComponentB from "./components/ComponentB";
-// import ComponentC from "./components/ComponentC";
-// import { CountContext } from "./CountContext";
-// import CounterThree from "./components/CounterThree";
-// import CounterTwo from "./components/CounterTwo";
-// import CounterOne from "./components/CounterOne";
 
 function App() {
-  return (
-    <div className="App">
-      <DataFetchingTwo />
-      {/* <DataFetchingOne /> */}
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
